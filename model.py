ab8f85a3e5190555fea1819725ef8512bfbe1b63
Justmore5mins <justmore5mins@gmail.com>
2024-11-22 12:38:52 +0800

    add

--- model.py
+++ model.py
@@ -1,9 +1,11 @@
-from ultralytics import YOLO
-from multiprocessing import freeze_support
+import os
 
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+os.environ['TORCH_USE_CUDA_DSA'] = "1"
+
+from ultralytics import YOLO
 
 model = YOLO(r"C:\Users\admin\Desktop\yolo11n.pt")
 model.to("cuda")
 if __name__ == '__main__':
     results = model.train(data=r"C:\Users\admin\Desktop\Model\datasets\prepared\data.yaml", epochs=1000, imgsz=640,verbose=True)
-    #freeze_support()
